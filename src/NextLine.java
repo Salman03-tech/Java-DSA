f16b833c7a8b669b41e2bb4e3f5a49b82531a8b5
Salman <mdsallu040@gmail.com>
2025-10-16 20:15:47 +0530

    NextLine print

---
+++ src/NextLine.java
@@ -0,0 +1,8 @@
+public class NextLine {
+    public static void main(String args[]){
+        System.out.println("Hello"); //will print next word after one line space
+        System.out.print("Hello\n");// \n will plays same as println
+        System.out.println("Hi");
+        System.out.println("Hello\nWorld\n Welcome");
+    }
+}
